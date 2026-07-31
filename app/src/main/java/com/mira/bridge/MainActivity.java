c9c640cd6edb04d72b1346664e4f95523dccf11a
Eng-Fahim <97174456+Eng-Fahim@users.noreply.github.com>
2026-07-31 03:28:14 +0300

    Refactor MainActivity to include fragment classes

--- app/src/main/java/com/mira/bridge/MainActivity.java
+++ app/src/main/java/com/mira/bridge/MainActivity.java
@@ -4,47 +4,67 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.mira.bridge.ui.dashboard.DashboardFragment;
-import com.mira.bridge.ui.scan.ScanFragment;
-import com.mira.bridge.ui.studio.StudioFragment;
-import com.mira.bridge.ui.gates.GatesFragment;
-import com.mira.bridge.ui.settings.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNav = findViewById(R.id.bottomNavigation);
-
-        if (savedInstanceState == null) {
-            loadFragment(new DashboardFragment());
-        }
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+        
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragmentContainer, new DashboardFragment())
+            .commit();
 
         bottomNav.setOnItemSelectedListener(item -> {
-            Fragment fragment = null;
+            Fragment f = null;
             int id = item.getItemId();
-            if (id == R.id.nav_dashboard) fragment = new DashboardFragment();
-            else if (id == R.id.nav_scan) fragment = new ScanFragment();
-            else if (id == R.id.nav_studio) fragment = new StudioFragment();
-            else if (id == R.id.nav_gates) fragment = new GatesFragment();
-            else if (id == R.id.nav_settings) fragment = new SettingsFragment();
-            
-            if (fragment != null) { loadFragment(fragment); return true; }
-            return false;
+            if (id == R.id.nav_dashboard) f = new DashboardFragment();
+            else if (id == R.id.nav_scan) f = new ScanFragment();
+            else if (id == R.id.nav_studio) f = new StudioFragment();
+            else if (id == R.id.nav_gates) f = new GatesFragment();
+            else if (id == R.id.nav_settings) f = new SettingsFragment();
+            if (f != null) getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, f).commit();
+            return true;
         });
     }
+}
 
-    public void loadFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction()
-            .replace(R.id.fragmentContainer, fragment).commit();
+class DashboardFragment extends Fragment {
+    public DashboardFragment() {}
+    @Override public View onCreateView(LayoutInflater i, ViewGroup c, Bundle b) {
+        View v = i.inflate(R.layout.fragment_dashboard, c, false);
+        ((TextView)v.findViewById(R.id.tvGreeting)).setText("مرحباً بك");
+        return v;
     }
+}
 
-    public void navigateTo(int menuId) {
-        bottomNav.setSelectedItemId(menuId);
+class ScanFragment extends Fragment {
+    public ScanFragment() {}
+    @Override public View onCreateView(LayoutInflater i, ViewGroup c, Bundle b) {
+        return i.inflate(R.layout.fragment_scan, c, false);
+    }
+}
+
+class StudioFragment extends Fragment {
+    public StudioFragment() {}
+    @Override public View onCreateView(LayoutInflater i, ViewGroup c, Bundle b) {
+        return i.inflate(R.layout.fragment_studio, c, false);
+    }
+}
+
+class GatesFragment extends Fragment {
+    public GatesFragment() {}
+    @Override public View onCreateView(LayoutInflater i, ViewGroup c, Bundle b) {
+        return i.inflate(R.layout.fragment_gates, c, false);
+    }
+}
+
+class SettingsFragment extends Fragment {
+    public SettingsFragment() {}
+    @Override public View onCreateView(LayoutInflater i, ViewGroup c, Bundle b) {
+        return i.inflate(R.layout.fragment_settings, c, false);
     }
 }
