b539579a3bbb112bceddc3ac874ad282babd06f0
Eng-Fahim <97174456+Eng-Fahim@users.noreply.github.com>
2026-07-31 03:15:19 +0300

    Add API client and preferences manager accessors
    Added methods to retrieve API client and preferences manager.

--- app/src/main/java/com/mira/bridge/MiraApp.java
+++ app/src/main/java/com/mira/bridge/MiraApp.java
@@ -1,16 +1,24 @@
 package com.mira.bridge;
 
 import android.app.Application;
+import com.mira.bridge.api.MiraApiClient;
+import com.mira.bridge.data.PreferencesManager;
 
 public class MiraApp extends Application {
     
     private static MiraApp instance;
+    private MiraApiClient apiClient;
+    private PreferencesManager prefs;
 
     @Override
     public void onCreate() {
         super.onCreate();
         instance = this;
+        apiClient = new MiraApiClient();
+        prefs = new PreferencesManager(this);
     }
 
     public static MiraApp getInstance() { return instance; }
+    public MiraApiClient getApiClient() { return apiClient; }
+    public PreferencesManager getPrefs() { return prefs; }
 }
