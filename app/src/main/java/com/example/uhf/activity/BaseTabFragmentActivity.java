ab694f4252d7c509e3eb7fe82d68e979d8770544
Eng-Fahim <97174456+Eng-Fahim@users.noreply.github.com>
2026-08-03 04:21:58 +0300

    fix: update legacy package imports

--- app/src/main/java/com/example/uhf/activity/BaseTabFragmentActivity.java
+++ app/src/main/java/com/example/uhf/activity/BaseTabFragmentActivity.java
@@ -20,7 +20,7 @@ import androidx.fragment.app.FragmentActivity;
 
 import com.example.uhf.BuildConfig;
 import com.example.uhf.R;
-import com.mira.rfid.fragment.KeyDwonFragment;
+import com.mira.rfid.KeyDwonFragment; // ✅ تغيير المسار;
 import com.mira.ui.utils.UIHelper;
 import com.rscja.deviceapi.RFIDWithUHFUART;
 import com.rscja.utility.StringUtility;
